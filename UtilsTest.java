ac6fd29ac359aa38b3174b2895b5a9d0a3394159
jbonettivalencia <chbonetti@gmail.com>
2025-10-06 20:14:11 -0400

    Rename test method for Maven

--- UtilsTest.java
+++ UtilsTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class UtilsTest {
     @Test
-    void sumAddsNumbers() {
+    void testsumAddsNumbers() {
         assertEquals(5, com.example.Utils.sum(2, 3));
     }
 }
